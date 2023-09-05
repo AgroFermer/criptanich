ef24859296a3e66121508e9361bc51493aa3c9fb
Lemishko Dmitry <81725793+AgroFermer@users.noreply.github.com>
2023-09-05 15:30:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ async def approve_request_bot1(chat_join: ChatJoinRequest, bot: Bot):
 # Bot 2 logic
 async def approve_request_bot2(chat_join: ChatJoinRequest, bot: Bot):
     text = hlink('https://t.me/globalprofit', 'https://t.me/+YigHdJ6h1ck3YmQy')
-   msg= f"Дорогой друг, поздравляем тебя с приобретением подписки на закрытое сообщество PROFIT.\n\nВы можете войти в канал: {text}"
-   button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+eWU1DCA0sEdhYjk6', disable_web_page_preview=True)   
-   markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
+    msg= f"Дорогой друг, поздравляем тебя с приобретением подписки на закрытое сообщество PROFIT.\n\nВы можете войти в канал: {text}"
+    button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+eWU1DCA0sEdhYjk6', disable_web_page_preview=True)   
+    markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
 
     await bot.send_message(chat_id=chat_join.from_user.id, text=msg, reply_markup=markup, disable_web_page_preview=True)
