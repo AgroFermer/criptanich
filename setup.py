20021317926d23893a8eb08c12cd8cd741b50499
Lemishko Dmitry <81725793+AgroFermer@users.noreply.github.com>
2023-09-05 17:45:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ async def approve_request_bot2(chat_join: ChatJoinRequest, bot: Bot):
     button = InlineKeyboardButton(text='ВСТУПИТИ', url='https://t.me/+4uMnS-VXsvg3M2Yy', disable_web_page_preview=True)
     markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
-    await bot.send_message(chat_id=chat_join.from_user.id, text=msg, parse_mode=types.ParseMode.HTML, reply_markup=markup, disable_web_page_preview=True)
+    await bot.send_message(chat_id=chat_join.from_user.id, text=msg, parse_mode="HTML", reply_markup=markup, disable_web_page_preview=True)
 
 
 async def start():
